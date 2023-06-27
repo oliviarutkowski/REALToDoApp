c0b1e813a60609e38bea9c08665239b03208cdae
Olivia Rutkowski <oliviarutkowski08@gmail.com>
2023-06-27 10:45:07 -0600

    Initial Commit

---
+++ REALToDoApp/REALToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  REALToDoAppApp.swift
+//  REALToDoApp
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct REALToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
